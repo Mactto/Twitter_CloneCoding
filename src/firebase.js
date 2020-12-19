bff06c0c7a1d9d6a05e7948696dbb9ff0e4ad3da
mactto <shc9928@gmail.com>
2020-12-19 16:12:22 +0900

    updates

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAizMA5IeqgOpiKL7ltRZpPh3iY1UuzZQw",
+    authDomain: "nwitter-a6061.firebaseapp.com",
+    projectId: "nwitter-a6061",
+    storageBucket: "nwitter-a6061.appspot.com",
+    messagingSenderId: "819184161042",
+    appId: "1:819184161042:web:5dd420839c288081e0a3cb",
+    measurementId: "G-CSQ21G0WS2"
+};
+
+export default firebase.initializeApp(firebaseConfig);
