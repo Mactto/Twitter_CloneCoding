61e1d6da3e126be128c0e4ffa321e58b3a0e7d70
mactto <shc9928@gmail.com>
2020-12-20 16:27:02 +0900

    auth1

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AppRouter from './Router';
+import {authService} from "../firebase";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  
   return (
-    <AppRouter />
-  );
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+    </>
+  )
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
+import { authService } from "../firebase";
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+
+    const onChange = (e) => {
+        const {target: {name, value}} = e;
+        if (name === "email") {
+            setEmail(e.currentTarget.value);
+        } else if (name === "password") {
+            setPassword(e.currentTarget.value);
+        }
+    }
+    const onSubmit = async(e) => {
+        e.preventDefault();
+        let data;
+        try {
+            if (newAccount) {
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange}/>
+                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>   
+    )
+}
+
 export default Auth;
