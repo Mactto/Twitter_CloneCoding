efff2a6c94419e966271202bbfa1c764fd397518
mactto <shc9928@gmail.com>
2020-12-20 17:34:01 +0900

    SignIn_CreateUser

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 트위터 클론코딩
+
+---
+
+### 🔎 Lecture Infomation
+
+🧑🏻‍🎤 Teacher : Nomad Coder  
+⏩⏩ Lecture Link : [Twitter Clone Coding](https://nomadcoders.co/nwitter/lobby)  
+💡 Explanation : How to use Firebase with React Hooks
+
+---
+
+### 💻 Developer Environment 🔌
+
+[React JS](https://ko.reactjs.org/) + [Firebase](https://firebase.google.com)
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
 
     const onChange = (e) => {
         const {target: {name, value}} = e;
@@ -25,9 +26,11 @@ const Auth = () => {
             }
             console.log(data);
         } catch(err) {
-            console.log(err);
+            setError(err.message);
         }
     }
+
+    const toggleAccount = () => setNewAccount(prev => !prev)
 
     return (
         <div>
@@ -36,10 +39,12 @@ const Auth = () => {
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
                 <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
             </form>
+            <span onClick={toggleAccount}>{newAccount ? "Log in" : "Create Account"}</span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
             </div>
+            {error}
         </div>   
     )
 }
