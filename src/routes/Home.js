b5edd43f431ee774e8e746d10a5fd776342b98cf
mactto <shc9928@gmail.com>
2020-12-20 22:26:44 +0900

    firestore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+import { dbService } from "../firebase";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await dbService.collection("nweets").add({
+            nweet,
+            createdAt: Date.now(),
+        });
+        setNweet("")
+    }
+    const onChange = (e) => {
+        const {target: {value}} = e;
+        setNweet(value);
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value="Nweet" />
+            </form>
+        </div>
+    )
+}
 export default Home;
